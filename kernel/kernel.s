dc5fd2d72ac1a1c90f25eb1e2ab1f879adb2ae56
WalonLi <a63241924@gmail.com>
2015-05-19 01:34:02 +0800

    First C code

--- kernel/kernel.s
+++ kernel/kernel.s
@@ -6,14 +6,31 @@
 
 .code32
 
-.global _start
+.extern     cstart
+.extern     gdt_pdr
+
+
+.bss
+#.lcomm    StackSpace,     1024*2
+StackSpace:     .space      1024*2,        0
+StackTop:
+
 
 .text
+
+.global     _start
 
 _start:
     mov     $0xf,       %ah
     mov     $'K',       %al
     mov     %ax,        %gs:((80*21+39)*2)
+
+    mov     $StackTop,   %esp
+    sgdtw   gdt_ptr
+    call    cstart
+    lgdtw   gdt_ptr
+
     jmp     .
+
 
 
---
+++ kernel/start.c
@@ -0,0 +1,19 @@
+
+
+
+int gdt_ptr[6] ;
+
+void cstart()
+{
+
+    while (1) ;
+    char t[2], a[2];
+    t[0] = 0xf ;
+    t[1] = 'F' ;
+
+    // memcpy(t, a, 2) ;
+
+
+    //while(1) ;
+
+}
